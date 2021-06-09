3ecb06b9503645bdfb0fc10c0e35d69b2a2f113f
YauheniBulitski <yauheni.bulitski@gmail.com>
2021-06-09 23:03:11 +0300

    create test

--- src/test/java/com/bulitski/app/service/UserServiceImplTest.java
+++ src/test/java/com/bulitski/app/service/UserServiceImplTest.java
@@ -1,33 +1,44 @@
 package com.bulitski.app.service;
 
 
+import com.bulitski.app.converter.UserDetailsConverter;
 import com.bulitski.app.model.Role;
 import com.bulitski.app.model.Status;
 import com.bulitski.app.model.UserAccount;
 import com.bulitski.app.repository.UserAccountRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
 
-    @Mock
-    UserAccountRepository userAccountRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @InjectMocks
-    private static UserServiceImpl userService;
+    private UserServiceImpl userServiceImpl;
+    private UserAccountRepository userAccountRepository;
+
+
+    @BeforeEach
+    public void init(){
+        userAccountRepository= mock(UserAccountRepository.class);
+        UserDetailsConverter userDetailsConverter= mock(UserDetailsConverter.class);
+        userServiceImpl=new UserServiceImpl(userAccountRepository,userDetailsConverter,passwordEncoder);
+    }
 
     @Test
     public void findByIdTest(){
         Long id= 1L;
-        Optional<UserAccount> user= Optional.ofNullable(UserAccount.builder()
+                Optional<UserAccount> user= Optional.ofNullable(UserAccount.builder()
                 .id(id)
                 .userName("Admin")
                 .firstName("Petya")
@@ -35,8 +46,10 @@ public class UserServiceImplTest {
                 .status(Status.ACTIVE)
                 .role(Role.ADMIN)
                 .build());
-        doReturn(user).when(userAccountRepository).findById(id);
-        UserAccount newUser=userService.findById(id);
-        assertThat(newUser).isEqualTo(user.get());
+        when(userAccountRepository.findById(id)).thenReturn(user);
+        UserAccount userAccount=userServiceImpl.findById(id);
+        verify(userAccountRepository, times(1)).findById(id);
+        assertThat(userAccount).isEqualTo(user.get());
     }
+
 }
